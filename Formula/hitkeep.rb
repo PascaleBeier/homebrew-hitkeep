a2e7478f705091b59b674564c27a3ccde3908d31
Pascale Beier <mail@pascalebeier.de>
2026-08-31 21:52:40 +0200

    chore: update HitKeep to 2.13.12

--- Formula/hitkeep.rb
+++ Formula/hitkeep.rb
@@ -1,8 +1,8 @@
 class Hitkeep < Formula
   desc "Privacy-friendly, self-hosted web analytics"
   homepage "https://hitkeep.com"
-  url "https://github.com/PascaleBeier/hitkeep/archive/refs/tags/v2.13.11.tar.gz"
-  sha256 "571cbdca02bd3d2731c57f9c9fdd67ee23ad7eb02b8b7f4aa8edebb5e89e28b3"
+  url "https://github.com/PascaleBeier/hitkeep/archive/refs/tags/v2.13.12.tar.gz"
+  sha256 "782d6ad78216d0f886e267dd71f02efd2070f65a3cf2e61eac6f10d20d0ef8a3"
   license "MIT"
 
   depends_on "go" => :build
